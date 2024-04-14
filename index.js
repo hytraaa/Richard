9fcc2bab43548b63c3a686cdf3c83d1cd51c0b6e
hytra <140700975+hytraaa@users.noreply.github.com>
2024-04-14 17:36:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let porte = 100;
+let maVariable = 15;
+let result = porte + maVariable;
+let cornichon = 10;
+let cornifion = "10";
+function maFirstFonction() {
+  console.log("jesuisunjambon");
+  console.log(10 ** 2);
+}
+maFirstFonction();
+{
+  alert(10);
+}
+const addition = (a, b) => {
+  console.log(a + b);
+};
+addition(8734587314, 923747509);
